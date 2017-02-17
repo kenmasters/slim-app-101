331c1177f3db70ffd1de46e6055d28f99da04140
Ken <Ken@ATserver.local>
2017-02-17 17:01:56 +0800

    Testing slim/pdo package.

--- classes/TicketMapper.php
+++ classes/TicketMapper.php
@@ -7,32 +7,59 @@ class TicketMapper {
 	}
 
 	function index() {
-		
-		$stmt = $this->pdo->prepare('SELECT * FROM tickets');
+		$params = [];
+		$q = 'SELECT * FROM tickets WHERE 1';
+
+		// $stmt = $this->pdo->prepare('SELECT * FROM tickets');
 		
 		if (isset($_GET['name']) && $_GET['name'] ) {
-			$stmt = $this->pdo->prepare('SELECT * FROM tickets WHERE name = :name');
-			$stmt->bindParam(':name', $_GET['name']);
+
+			$params['name'] = $_GET['name'];
+			// $stmt = $this->pdo->prepare('SELECT * FROM tickets WHERE name = :name');
+			// $stmt->bindParam(':name', $_GET['name']);
+		}
+
+		if( $params ) {
+			foreach ($params as $key => $value) {
+				$q .= sprintf(' AND `%s` = :%s' , $key, $key);
+			}
+		}
+
+		$stmt = $this->pdo->prepare($q);
+
+		if( $params ) {
+			foreach ($params as $key => $value) {
+				$stmt->bindParam(':'.$key, $value);
+			}
 		}
 
 		$stmt->execute();
 		$tickets = $stmt->fetchAll();
-		return json_encode($tickets);
+		// return json_encode($tickets);
+		return $tickets;
 	}
 
 	function show($id) {
 		$stmt = $this->pdo->prepare('SELECT * FROM tickets WHERE id = :id');
 		$stmt->execute(['id' => $id]);
-		$ticket = $stmt->fetch();
-		return json_encode($ticket);
+		$ticket = $stmt->fetchObject();
+		// return json_encode($ticket);
+		return $ticket;
 	}
 
-	function getTickets() {
-		$stmt = $this->pdo->prepare('SELECT * FROM tickets');
-		$stmt->execute();
-		$tickets = $stmt->fetchAll();
-		return json_encode($tickets);
+	function store() {
+		$input = $request->getParsedBody();
+		$sql = "INSERT INTO tickets (name, created_at, updated_at) VALUES (:name, NOW(), NOW())";
+		 $sth = $this->db->prepare($sql);
+		$sth->bindParam("name", $input['name']);
+		$sth->execute();
+		$input['id'] = $this->db->lastInsertId();
+		return $response->withJson($input);
 	}
+	
+	function update($id) {}
+	function destroy($id) {}
+
 }
 
 // $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND status=?');
--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-use Psr7Middlewares\Middleware\TrailingSlash;
 
 require 'vendor/autoload.php';
 
-
-date_default_timezone_set('Australia/Perth'); // Set timezone
+// date_default_timezone_set('America/Chicago'); // Set timezone
+date_default_timezone_set('Asia/Manila'); // Set timezone
 
 spl_autoload_register(function ($classname) {
     require ("classes/" . $classname . ".php");
 });
 
-// $config['displayErrorDetails'] = true;
-// $config['addContentLengthHeader'] = false;
-
-// $config['db']['host']   = "192.185.81.207"; // Remote database conn
-// $config['db']['dbname'] = "wowprobl_slim";
-// $config['db']['user']   = "wowprobl_slim";
-// $config['db']['pass']   = "du(&Ab2&Zmo1";
-
-// wowprobl_slim du(&Ab2&Zmo1
 $config = require 'config.php';
 $app = new Slim\App($config);
-
 $container = $app->getContainer();
 
+
+ /* DEPENDENCY INJECTION */
 $container['logger'] = function($container) {
     $logger = new \Monolog\Logger('my_logger');
     $file_handler = new \Monolog\Handler\StreamHandler("logs/app.log");
@@ -36,39 +27,29 @@ $container['logger'] = function($container) {
 
 $container['db'] = function ($container) {
     $db = $container['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-        $db['user'], $db['pass']);
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
-// $app->add(new TrailingSlash(true)); 
 
-// $app->add(function (Request $request, Response $response, callable $next) {
-//     $uri = $request->getUri();
-//     $path = $uri->getPath();
-//     if ($path != '/' && substr($path, -1) == '/') {
-//         // permanently redirect paths with a trailing slash
-//         // to their non-trailing counterpart
-//         $uri = $uri->withPath(substr($path, 0, -1));
-        
-//         if($request->getMethod() == 'GET') {
-//             return $response->withRedirect((string)$uri, 301);
-//         }
-//         else {
-//             return $next($request->withUri($uri), $response);
-//         }
-//     }
 
-//     return $next($request, $response);
-// });
+$container['DB'] = function ($container) {
+    $db = $container['settings']['db'];
+    $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+    $usr = $db['user'];
+    $pwd = $db['pass'];
+    $pdo = new \Slim\PDO\Database($dsn, $usr, $pwd);
+    return $pdo;
+};
 
+
+
+/* APPLICATION ROUTE/ENDPOINTS */
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
-
     return $response;
-
 });
 
 $app->get('/', function ($request, $response, $args) {
@@ -76,38 +57,98 @@ $app->get('/', function ($request, $response, $args) {
     return $response->write("Silence is a woman's best garment. (anonymous)");
 });
 
-// $app->get('/tickets', function (Request $request, Response $response) {
-//     $this->logger->addInfo("Ticket list");
-//     $mapper = new TicketMapper($this->db);
-//     $tickets = $mapper->getTickets();
-//     $response->getBody()->write($tickets);
-//     return $response;
-// });
 
+$app->get('/docs', function(Request $request, Response $response) {
+    echo '<h3>This docs is for testing Slim Framework API</h3>';
+    echo "<pre>";   
 
-// Tickets
+    print_r ($request->getUri());
+    echo '<br/>';
+
+    print_r ($request->getMethod());
+    echo '<br/>';
+
+    print_r ($request->getUri()->getQuery());
+    echo '<br/>';
+
+    print_r ($request->getQueryParams());
+    echo '<br/>';
+
+    print_r ($request->getQueryParam('age', null));
+    echo '<br/>';
+
+    return '';
+});
+
+$app->get('/slim-pdo', function (Request $request, Response $response) {
+    // SELECT * FROM users WHERE id = ?
+    $selectStatement = $this->DB->select()->from('tickets');
+
+    $id = $request->getQueryParam('id');
+    $name = $request->getQueryParam('name');
+    
+
+    if( $id ) {
+        $selectStatement->where('id', '=', $id);
+    }
+
+    if( $name ) {
+        $selectStatement->whereLike('name', "$name%");
+    }
+
+    $stmt = $selectStatement->execute();
+    $data = $stmt->fetchAll();
+    return $response->withJson($data);
+});
+
 $app->group('/tickets', function(){
 
     $this->get('', function(Request $request, Response $response){
         $this->logger->addInfo("Ticket list");
         $mapper = new TicketMapper($this->db);
         $tickets = $mapper->index();
-        $response->getBody()->write($tickets);
-        return $response;
+        return $response->withJson($tickets);
     });
+
+    // Add a new ticket
+    $this->post('', function ($request, $response) {
+       $input = $request->getParsedBody();
+       echo $input['name'];
+    });
+    // - See more at: https://arjunphp.com/creating-restful-api-slim-framework/#sthash.Dyxd4GPx.dpuf
 
     $this->get('/{id}', function(Request $request, Response $response){
         $this->logger->addInfo("Ticket Info");
         $mapper = new TicketMapper($this->db);
         $ticket = $mapper->show($request->getAttribute('id'));
-        $response->getBody()->write($ticket);
-        return $response;
+        return $response->withJson($ticket);
     });
 
+    // DELETE a todo with given id
+    $this->delete('/{id}', function ($request, $response, $args) {
+        $sth = $this->db->prepare("DELETE FROM tickets WHERE id=:id");
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
+        $todos = $sth->rowCount();
+        return $response->withJson($todos);
+    });
+    // - See more at: https://arjunphp.com/creating-restful-api-slim-framework/#sthash.Dyxd4GPx.dpuf
+    // Update todo with given id
+    $this->put('/{id}', function ($request, $response, $args) {
+        $input = $request->getParsedBody();
+        $sql = "UPDATE tickets SET name=:name WHERE id=:id";
+        $sth = $this->db->prepare($sql);
+        $sth->bindParam("id", $args['id']);
+        $sth->bindParam("name", $input['name']);
+        $sth->execute();
+        $input['id'] = $args['id'];
+        return $response->withJson($input);
+    });
+    // - See more at: https://arjunphp.com/creating-restful-api-slim-framework/#sthash.Dyxd4GPx.dpuf
 });
 
 
-
+/* RUN THE APPLICATION */ 
 $app->run();
 
 
